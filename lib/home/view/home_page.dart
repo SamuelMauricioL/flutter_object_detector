f62a5ea026bcbf7874c42a0b9a2fed30922ee204
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-06-13 18:14:42 -0500

    chore: home page images

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -11,21 +11,48 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  static String tfImage =
+      'https://imgs.search.brave.com/t4QoHy_Dp2Vbqc539qlOgRt_i58hrKh6ANIb97qDFOw/rs:fit:960:536:1/g:ce/aHR0cHM6Ly9zcGVj/aWFscy1pbWFnZXMu/Zm9yYmVzaW1nLmNv/bS9pbWFnZXNlcnZl/LzVmYzA5Y2YxOTUw/MTc5YTNmNWY3NDg3/NC85NjB4MC5qcGc_/Zml0PXNjYWxl';
+
+  static String flutterImage =
+      'https://imgs.search.brave.com/5qDql1Jd_J7Zr1Jo2ivgi1a6y0MLci7LIoWH8Y0OQLI/rs:fit:1124:533:1/g:ce/aHR0cHM6Ly90ZWNo/LnBlbG1vcmV4LmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8xMC9mbHV0dGVy/LnBuZw';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                const toDetector = AppPageChangedTo(
-                  page: AppPageStatus.detector,
-                );
-                context.read<AppBloc>().add(toDetector);
-              },
-              child: const Text('Real Time Detection'),
+            Image.network(tfImage, fit: BoxFit.fill),
+            const SizedBox(height: 20),
+            const Text('+', style: TextStyle(fontSize: 50)),
+            const SizedBox(height: 20),
+            Image.network(flutterImage, fit: BoxFit.fill),
+            const SizedBox(height: 50),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      const toDetector = AppPageChangedTo(
+                        page: AppPageStatus.detector,
+                      );
+                      context.read<AppBloc>().add(toDetector);
+                    },
+                    child: const Text(
+                      'Real Time Detection',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'By: Samuel Mauricio Laime 💙',
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
